e0d1ab77c707d10d47a45bfdc931b5981167a7ab
Demo PC Manyone <developer@manyone.com>
2023-06-02 10:47:39 +0200

    Renamed camer animator prefab to Camera Positioner

--- Assets/_Presenter/Scripts/CameraPositioner.cs
+++ Assets/_Presenter/Scripts/CameraPositioner.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraAnimator : MonoBehaviour
+public class CameraPositioner : MonoBehaviour
 {
     GameObject projectorsCameras;
     Vector3 tempRotation;
